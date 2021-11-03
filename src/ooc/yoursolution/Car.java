b793d18585ea07d2d8e7877af5b9061c79aa43d5
aurelio <aurelio_a10@hotmail.com>
2021-11-03 12:50:00 +0000

    Signed value for attributes, getters and setters, method implementation to book the car if available

--- src/ooc/yoursolution/Car.java
+++ src/ooc/yoursolution/Car.java
@@ -5,6 +5,7 @@
  */
 package ooc.yoursolution;
 
+import java.util.HashMap;
 import java.util.Map;
 import ooc.enums.Make;
 import ooc.enums.Month;
@@ -15,54 +16,67 @@ import ooc.enums.Month;
  */
 public class Car implements CarInterface {
 
+    private int id;             
+    private Make make;
+    private Map<Month, boolean[]>availability;
+    private double dailyRate;
+    //attributes. Valued signed to create setters and getter
+    
     @Override
     public Map<Month, boolean[]> createAvailability() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        availability = new HashMap<>();
+        return availability;
     }
 
     @Override
     public Make getMake() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return make;                         //retrieve the value from the property "make"
     }
 
     @Override
     public void setMake(Make make) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.make = make;                           //setting the values for the attribute "make"
     }
 
     @Override
     public double getRate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.dailyRate;                     //retrieve the value from the property "dailyrate"
     }
 
     @Override
-    public void setRate(double rate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setRate(double rate) { 
+        this.dailyRate = rate;                   //setting the values for the attribute "rate"
     }
 
     @Override
-    public Map<Month, boolean[]> getAvailability() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Map<Month, boolean[]> getAvailability() {       //retrieve the value from the property "availability"
+        return this.availability;                
     }
 
     @Override
     public void setAvailability(Map<Month, boolean[]> availability) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.availability = availability;                  //setting the values for the attribute "availability"
     }
 
     @Override
     public int getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.id;                     //retrieve the values from the attribute "id"
     }
 
     @Override
-    public boolean isAvailable(Month month, int day) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean isAvailable(Month month, int day) {           
+        return this.availability.get(month)[day-1];           //retrieve the value from the propertie "make"
     }
 
     @Override
     public boolean book(Month month, int day) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(!availability.get(month)[day-1]){                //if statement to book cars if available monthly/daily 
+            availability.get(month)[day-1] = true;
+    }
+        
+        return availability.get(month)[day-1];               //to book a car if availability is true
+        
+        
     }
     
 }
